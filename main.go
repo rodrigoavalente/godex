2e4a3a4137ff58d84dac5505f3a726f658b8aa69
Rodrigo Valente <rodrigoavrav@gmail.com>
2016-01-09 18:10:36 -0300

    Added simple query

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/rodrigoavalente/gopokemon"
 	"net/http"
+	"strings"
 )
 
 func Hello(writer http.ResponseWriter, request *http.Request) {
@@ -9,6 +12,16 @@ func Hello(writer http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", Hello)
+	http.HandleFunc("/pokemon/", func(writer http.ResponseWriter, request *http.Request) {
+		pokemon_id := strings.SplitN(request.URL.Path, "/", 3)[2]
+		pokemon, err := gopokemon.QueryPokemon(pokemon_id)
+		if err != nil {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		writer.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(writer).Encode(pokemon)
+	})
 	http.ListenAndServe(":8080", nil)
 }
